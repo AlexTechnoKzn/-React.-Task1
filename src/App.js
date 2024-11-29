cd37f0608da097c894b47fc875a495cd4801415b
Alex <noclafsake50@gmail.com>
2024-11-29 03:05:41 +0300

    USE JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-// Декларированный
+import { createElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// Императивный
+
 export const App = () => {
-	return (
-		// Декларированный
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+	const year = new Date().getFullYear();
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('div', null, year),
+		),
 	);
 };
